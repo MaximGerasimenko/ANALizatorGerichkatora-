1193c89f6460d8708cee8f49864a51a43c4fe1a3
MaximGerasimenko <lux3rush@gmail.com>
2021-06-30 17:17:12 +0200

    Signed-off-by: MaximGerasimenko <lux3rush@gmail.com>

--- ANALizatorGerichkatora/views.py
+++ ANALizatorGerichkatora/views.py
@@ -1,7 +1,17 @@
 import os
 
+from flask import Flask, request, abort
 from django.shortcuts import render, redirect
 
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print(request.json)
+        return 'success', 200
+    else:
+        abort(400)
 
 def main_redirect_view(request):
     print('REDIRECT FROM MAIN TO MAIN')
